b60bbfd7a7c3c9fe60bd78bc2a01d79d5eebace8
nomos244 <nomos.geek@gmail.com>
2022-05-06 12:52:58 +0900

    Add ActionMailer for notification

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'notice@qa-sample-app.com'
   layout 'mailer'
 end
---
+++ app/mailers/qa_mailer.rb
@@ -0,0 +1,23 @@
+class QaMailer < ApplicationMailer
+
+  def question_notification(question)
+    @question = question
+    @emails = User.where.not(id: question.user_id).pluck(:email)
+
+    mail bcc: @emails,
+         subject: "質問が作成されました"
+
+
+  end
+
+  def answer_notification(answer)
+    @answer = answer
+    user_ids = answer.question.answers.where.not(user_id: answer.user_id).pluck(:user_id) + [answer.question.user_id]
+    @emails = User.where(id: user_ids).pluck(:email)
+
+    mail bcc: @emails,
+         subject: "質問が作成されました"
+
+
+  end
+end
